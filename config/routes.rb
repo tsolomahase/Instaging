ecc6d130cdd311b83c4a2bba0f8b023a0c7781ef
tsolomahase <mahase@iuj.ac.jp>
2020-10-30 06:00:06 +0000

    Fixing the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'users#new'
+
+  resources :sessions,only: [:new , :create, :destroy]
+  get "/sessions/:id", to: "sessions#destroy"
+  post "/sessions/new", to: "users#index"
+
+  post "/posts/create", to: "posts#confirm"
+
+  resources :users do
+    member do
+      get :favourite
+    end
+  end
+
+  resources :posts ,only: [:new ,:show , :update , :confirm, :edit, :index, :create, :destroy] do
+  get "/post/:id", to: "post#destroy"
+    collection do
+      post :confirm
+    end
+
+  end
+resources :favorites, only: [:create, :destroy]
+# get "/favorites/:id", to: "favorite#destroy"
+
+mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
