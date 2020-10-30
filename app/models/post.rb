9e75911f331a2080e1ac227047cffa197651ce8c
tsolomahase <mahase@iuj.ac.jp>
2020-10-30 05:42:44 +0000

    models - post & user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+belongs_to :user
+has_many :photos, dependent: :destroy
+
+has_many :favorites, dependent: :destroy
+has_many :favorite_users, through: :favorites, source: :user
+
+mount_uploader :image, PhotoUploader
+
+validates :image, presence: true
+validates :content, presence: true, length: { maximum: 140 }
+
+def is_belongs_to? user
+  Post.find_by(user_id: user.id, id: id)
+end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+has_secure_password
+
+has_many :posts, dependent: :destroy
+has_many :favorites, dependent: :destroy
+has_many :favorite_posts, through: :favorites, source: :post
+
+mount_uploader :icon, IconUploader
+
+  validates :icon, presence: true
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /@/
+
+
+
 end
