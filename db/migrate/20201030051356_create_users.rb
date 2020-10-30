46029272bbc61b9dc1064ee5aa27e000f39240e4
tsolomahase <mahase@iuj.ac.jp>
2020-10-30 05:22:31 +0000

    gen the favorite model

--- db/migrate/20201030051356_create_users.rb
+++ db/migrate/20201030051356_create_users.rb
@@ -1,8 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+      t.string "name"
+        t.string "email"
+        t.string "password_digest"
+        t.string "image_link"
+        t.string "provider"
+        t.integer "user_id"
 
-      t.timestamps
+        t.timestamps null: false
     end
   end
 end
