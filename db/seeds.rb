a6f4c210e3469182dd0b625ecd83b1228003a837
tsolomahase <mahase@iuj.ac.jp>
2020-11-02 16:50:20 +0000

    fix files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do |n|
+  name = Faker::TvShows::TheITCrowd.character #=> "Roy Trenneman"
+  email = Faker::TvShows::TheITCrowd.email #=> "roy.trenneman@reynholm.test"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               )
+end
